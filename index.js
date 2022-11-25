3a3df7c38cbf4488e774b2c2ca2ac7c9e1cd2333
suhanasalma <suhanasalma800@gmail.com>
2022-11-25 17:12:45 +0600

    products added success

--- index.js
+++ index.js
@@ -19,90 +19,114 @@ const client = new MongoClient(uri, {
 
 async function run(){
    try{
+     const categoryCollection = client
+       .db("FurnitureShop")
+       .collection("FurnitureCategory");
+     const productsCollection = client
+       .db("FurnitureShop")
+       .collection("AllProducts");
+     const usersCollection = client.db("FurnitureShop").collection("Users");
 
-      const categoryCollection = client.db("FurnitureShop").collection("FurnitureCategory");
-      const productsCollection = client.db("FurnitureShop").collection("AllProducts");
-      const usersCollection = client
-        .db("FurnitureShop")
-        .collection("Users");
+     app.get("/categories", async (req, res) => {
+       const query = {};
+       const result = await categoryCollection.find(query).toArray();
+       res.send(result);
+     });
 
-      app.get('/categories',async(req,res)=>{
-         const query = {}
-         const result =await categoryCollection.find(query).toArray()
-         res.send(result)
-      })
+     app.get("/categories/:id", async (req, res) => {
+       const id = parseInt(req.params.id);
+       const query = { categoryId: id };
+       const result = await productsCollection.find(query).toArray();
+       res.send(result);
+     });
 
-      app.get('/categories/:id',async(req,res)=>{
-         const id = parseInt(req.params.id)
-         const query = { categoryId: id };
-         const result = await productsCollection.find(query).toArray()
-         res.send(result)
-      })
+     app.get("/productDetails/:id", async (req, res) => {
+       const id = req.params.id;
+       const query = { _id: ObjectId(id) };
+       const result = await productsCollection.findOne(query);
+       res.send(result);
+     });
 
-      app.get('/productDetails/:id',async(req,res)=>{
-         const id = req.params.id;
-         const query = {_id:ObjectId(id)}
-         const result = await productsCollection.findOne(query)
-         res.send(result)
+     //time added
 
-      })
+    //  app.get('/allProducts',async(req,res)=>{
+    //     const filter ={}
+    //     const option = {upsert:true}
+    //     const updateDoc = {
+    //       $set: {
+    //         postedTime: new Date(),
+    //       },
+    //     };
+    //     const result = await productsCollection.updateMany(filter,updateDoc,option)
+    //     res.send(result)
+    //  })
 
-      //time added
+    app.post("/allProducts", async (req, res) => {
+      const product = req.body;
+      const result = await productsCollection.insertOne(product)
+      res.send(result)
+      // console.log(product)
+    })
 
-      // app.get('/allProducts',async(req,res)=>{
-      //    const filter ={}
-      //    const option = {upsert:true}
-      //    const updateDoc = {
-      //       $set:{
-      //          postedTime: new Date()
-      //       }
-      //    }
-      //    const result = await productsCollection.updateMany(filter,updateDoc,option)
-      //    res.send(result)
-      // })
+     app.get("/allProducts", async (req, res) => {
+       const filter = {};
+       const page = parseInt(req.query.page);
+       const limit = parseInt(req.query.limit);
+       const result = await productsCollection
+         .find(filter)
+         .skip(page * limit)
+         .limit(limit)
+         .toArray();
+       const count = await productsCollection.estimatedDocumentCount();
+       res.send({ count, result });
+     });
 
+     app.get("/allProducts/advertise", async (req, res) => {
+       const filter = { advertised: true };
+       const page = parseInt(req.query.page);
+       const limit = parseInt(req.query.limit);
+       const result = await productsCollection
+         .find(filter)
+         .skip(page * limit)
+         .limit(limit)
+         .toArray();
+       const count = result.length;
+       res.send({ count, result });
+     });
 
-      app.get('/allProducts',async(req,res)=>{
-         const filter ={}
-         const page = parseInt(req.query.page);
-         const limit  = parseInt(req.query.limit);
-         const result = await productsCollection.find(filter).skip(page*limit).limit(limit).toArray()
-         const count = await productsCollection.estimatedDocumentCount()
-         res.send({count,result})
-      })
+    //  app.post("/users", async (req, res) => {
+    //    const user = req.body;
+    //    console.log(user);
+    //    const result = await usersCollection.insertOne(user);
+    //    res.send(result);
+    //  });
 
-       app.get("/allProducts/advertise", async (req, res) => {
-         const filter = {advertised:true}
-           const page = parseInt(req.query.page);
-           const limit = parseInt(req.query.limit);
-         const result = await productsCollection
-           .find(filter)
-           .skip(page * limit)
-           .limit(limit)
-           .toArray();
-         const count = result.length
-         res.send({ count, result });
-
-       });
-
-       app.post('/users',async(req,res)=>{
+      app.put("/users/:email", async (req, res)=>{
+        const email = req.params.email
+         const filter ={email:email}
          const user = req.body
-         console.log(user)
-         const result = await usersCollection.insertOne(user);
+         const option = {upsert:true}
+         const updateDoc = {
+            $set: user
+
+         }
+         const result = await usersCollection.updateOne(filter,updateDoc,option)
          res.send(result)
+      })
 
-       })
-       app.get('/users',async(req,res)=>{
-         const role = req.query.role
-         const query = {role:role};
-         const result  = await usersCollection.find(query).toArray()
-         // console.log(query)
-         res.send(result)
-
-       })
-
-
-
+     app.get("/users", async (req, res) => {
+       //  const role = req.query.role
+       let query = {};
+       if (req.query.role) {
+         query = { role: req.query.role };
+       } else {
+         query = { email: req.query.email };
+       }
+       //  const query = {role:role};
+       const result = await usersCollection.find(query).toArray();
+       // console.log(query)
+       res.send(result);
+     });
    }
    finally{
 
