41884f37a80eb19c28e1b57a610bc7d1480c0aa9
suhanasalma <suhanasalma800@gmail.com>
2022-12-04 00:37:07 +0600

    publishing

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const { query } = require("express");
 const port = process.env.PORT || 5000;
 const stripe = require("stripe")(process.env.STRIPE_SK);
 
+
+
 app.use(cors());
 app.use(express.json());
 
